a1e80bbf96c5f4ee44e3e6eaa480e8481ad1a3c3
kghosh19 <kathamrita19@gmail.com>
2026-05-01 18:21:27 +0530

    cleaning

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
-import os
+
 load_dotenv()
 
 
 def main():
     print("Hello from langchain-course!")
-    print(os.environ.get("OPEN_API_KEY"))
+    
 
 if __name__ == "__main__":
     main()
